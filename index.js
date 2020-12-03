381992906091d9a9ba35e609b504303aa9599ae1
Beyley Thomas <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-12-02 21:56:36 -0800

    Add ranking, just has userpage code for now

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ const app = express()
 // Render static files
 app.use(express.static('public'));
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
 const port = 999
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 const { response } = require('express');
 var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.name,
-  password : config.password,
-  database : config.database,
-  insecureAuth : true
+    host: config.host,
+    user: config.name,
+    password: config.password,
+    database: config.database,
+    insecureAuth: true
 });
 
 app.get('/u/:id', (req, res) => {
@@ -50,7 +50,7 @@ app.get('/u/:id', (req, res) => {
         });
 
         connection.query('SELECT * FROM osu_scores WHERE username = ? and pass = True ORDER BY score DESC', req.params.id, (err, results, fields) => {
-            if(err) throw err;
+            if (err) throw err;
             connection.query('SELECT * FROM osu_maps WHERE ranking = 2;', (err, results2, fields) => {
                 results.forEach(r2 => {
                     results2.forEach(r => {
@@ -61,24 +61,61 @@ app.get('/u/:id', (req, res) => {
                                 hashes.push(r2.osuhash);
                             }
                         }
-                        
+
                     });
                 });
-                        // Render index page
+                // Render index page
                 res.render('pages/user.ejs', {
                     // EJS variable and server-side variable
                     maps: maps,
                     scores: scores
                 });
             });
-            
-            
-
         });
-    } catch (err) {
-        
-    }
-    
+    } catch (err) { }
+})
+
+app.get('/ranking', (req, res) => {
+    try {
+        hashes = [];
+        scores = [];
+        maps = [];
+        text = "";
+
+        connection.query('SELECT * FROM osu_users ORDER BY rankedscore DESC;', req.params.id, (err, results, fields) => {
+            rank = 1
+            results.forEach(r => {
+                if (req.params.id.toLowerCase() == r.username.toLowerCase()) {
+                    text += r.username + " (#" + rank + ")\nranked score: " + r.rankedscore + " | total score: " + r.totalscore + "\naccuracy: " + r.accuracy + "%\nplaycount: " + r.playcount
+                }
+                rank++
+            })
+        });
+
+        connection.query('SELECT * FROM osu_scores WHERE username = ? and pass = True ORDER BY score DESC', req.params.id, (err, results, fields) => {
+            if (err) throw err;
+            connection.query('SELECT * FROM osu_maps WHERE ranking = 2;', (err, results2, fields) => {
+                results.forEach(r2 => {
+                    results2.forEach(r => {
+                        if (r2.osuhash == r.md5) {
+                            if (hashes.indexOf(r2.osuhash) == -1) {
+                                scores.push("score: " + r2.score + " | | " + r.name.split("|").join(" ") + "\n");
+                                maps.push(r.setid);
+                                hashes.push(r2.osuhash);
+                            }
+                        }
+
+                    });
+                });
+                // Render index page
+                res.render('pages/user.ejs', {
+                    // EJS variable and server-side variable
+                    maps: maps,
+                    scores: scores
+                });
+            });
+        });
+    } catch (err) { }
 })
 
 app.get('/register', (req, res) => {
@@ -91,15 +128,15 @@ app.post('/registerauth', (req, res) => {
         var password = md5(req.body.password).toString();
         var email = req.body.email;
         if (username && password && email) {
-            connection.query('INSERT INTO osu_users (email, username, password, playcount, totalscore, rankedscore, accuracy, s300, s100, s50, s0) VALUES (?, ?, ?, 0, 0, 0, 0, 0, 0, 0, 0)', [email, username, password], function(err, results, fields) {
+            connection.query('INSERT INTO osu_users (email, username, password, playcount, totalscore, rankedscore, accuracy, s300, s100, s50, s0) VALUES (?, ?, ?, 0, 0, 0, 0, 0, 0, 0, 0)', [email, username, password], function (err, results, fields) {
                 if (err) {
                     if (err.code == 'ER_DUP_ENTRY' || err.errno == 1062) {
                         res.send("error happened, username already in use");
-                        res.end(); 
+                        res.end();
                     } else {
                         console.log(err);
                         res.send("error happened, unknown one");
-                        res.end(); 
+                        res.end();
                     }
                 } else {
                     res.send("user is created go play")
